dbd8cc06812642766b0c0ee6b6a085b280d13065
Roman Hera <romahera6@gmail.com>
2022-07-11 19:23:47 +0300

    Add files via upload

---
+++ Zminni.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+public class Zminni {
+	public static void main(String[] args) {
+		int var1;
+		int var2;
+		
+		var1 = 4096;
+		var2 = var1 / 4;
+		
+		JOptionPane.showMessageDialog(null, "var1 = " + var1);
+		JOptionPane.showMessageDialog(null, "var2 = var1 / 4 = " + var2);
+	}
+
+}
